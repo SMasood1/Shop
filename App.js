437bea95b3043b8a64a85c1c6369c1a2b9fc47c4
Muhammad Masood <1masoodsha1@gmail.com>
2020-05-05 19:00:41 -0400

    Setup navigation

--- App.js
+++ App.js
@@ -2,20 +2,34 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Platform } from 'react-native'
 
 import productReducer from './store/reducers/products';
+import ProductsOverViewScreen from './components/shop/ProductsOverViewScreen';
+import Colors from './constants/colors';
 
 const rootReducer = combineReducers({
   products: productReducer,
 })
 
 const store = createStore(rootReducer);
-
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <Provider store={store}>
-      
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
+            headerTintColor: Platform.OS === 'android' ? 'white': Colors.primary,
+          }}
+        >
+          <Stack.Screen component={ProductsOverViewScreen}/>
+        </Stack.Navigator>   
+      </NavigationContainer>
     </Provider>
   );
 }
