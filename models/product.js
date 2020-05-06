3f3092605914426a666f379a9a403cb61ff9a9a7
Muhammad Masood <1masoodsha1@gmail.com>
2020-05-06 19:48:51 -0400

    fix(models/product.js): add missing parameter in constructor

--- models/product.js
+++ models/product.js
@@ -1,7 +1,8 @@
 class Product{
-    constructor(id, ownerId, imageUrl, description, price){
+    constructor(id, ownerId, title, imageUrl, description, price){
         this.id = id;
         this.ownerId = ownerId;
+        this.title = title;
         this.imageUrl = imageUrl;
         this.description = description;
         this.price = price;
