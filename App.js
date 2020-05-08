a284122aa6058ef0e33ff34b7e121028edef82bd
Muhammad Masood <1masoodsha1@gmail.com>
2020-05-08 07:44:12 -0400

    add(App.js,...): add custom fonts throughout app

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
+import { useFonts } from '@use-expo/font';
+import { AppLoading } from 'expo';
 
 import productReducer from './store/reducers/products';
 import StackNavigator from './navigation/StackNavigator'
@@ -13,12 +15,22 @@ const rootReducer = combineReducers({
 const store = createStore(rootReducer);
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
 
-  return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <StackNavigator/>
-      </NavigationContainer>
-    </Provider>
-  );
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <Provider store={store}>
+        <NavigationContainer>
+          {/* Getting an error because of StackNavigator*/}
+          <StackNavigator/>
+        </NavigationContainer>
+      </Provider>
+      
+    );
+  }
 }
--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Platform } from 'react-native'
+import { useSelector } from 'react-redux'
 
 import ProductsOverViewScreen from '../screens/shop/ProductsOverViewScreen';
 import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
@@ -9,36 +10,41 @@ import Colors from '../constants/colors';
 const Stack = createStackNavigator();
 
 const StackNavigator = ()=>{
-    return (
-        <Stack.Navigator
-          initialRouteName="ProductsOverView"
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
-            },
-            headerTintColor: Platform.OS === 'android' ? 'white': Colors.primary,    
-          }}
-        >
-            <Stack.Screen  
-            name='ProductsOverView' 
-            component={ProductsOverViewScreen}
-            options={{
-                title: 'All Products'
-            }}
-            />
-            <Stack.Screen
-            name='ProductDetail'
-            component={ProductDetailScreen}
-            options={(
-                ({route}) => ({
-                title: ()=>{
-                    route.params.productId
-                }
-                })
-            )}
-            />
-        </Stack.Navigator> 
-    )
+
+  return (
+    <Stack.Navigator
+      initialRouteName="ProductsOverView"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
+        },
+        headerTintColor: Platform.OS === 'android' ? 'white': Colors.primary,
+        headerTitleStyle: {
+          fontFamily: 'open-sans-bold'
+        } ,
+        headerBackTitleStyle: {
+          fontFamily: 'open-sans'
+        }
+      }}
+    >
+      <Stack.Screen  
+      name='ProductsOverView' 
+      component={ProductsOverViewScreen}
+      options={{
+          title: 'All Products'
+      }}
+      />
+      {/* Need to get title route.param.title */}
+
+      <Stack.Screen
+      name='ProductDetail'
+      component={ProductDetailScreen}
+      options={{
+        
+      }}
+      />
+    </Stack.Navigator> 
+  )
 }
 
 export default StackNavigator;
