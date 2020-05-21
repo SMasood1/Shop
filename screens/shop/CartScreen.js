a2fee833549722ddd87bded7a1896186ddad8e96
Muhammad Masood <1masoodsha1@gmail.com>
2020-05-21 18:37:28 -0400

    the checkout cart screen

---
+++ screens/shop/CartScreen.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { useSelector } from "react-redux";
+import Colors from "../../constants/Colors";
+
+const CartScreen = props => {
+  const cartTotalAmount = useSelector(state => state.cart.totalAmount);
+  return (
+    <View style={styles.screen}>
+      <View style={styles.summary}>
+        <Text style={styles.summeryText}>
+          Total: <Text style={styles.amount}>${cartTotalAmount}</Text>
+        </Text>
+        <Button title="Order Now"></Button>
+      </View>
+      <View>
+        <Text>Cart Iems</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    margin: 20
+  },
+  summary: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+    padding: 10,
+    shadowColor: 'black',
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: 'white'
+  },
+  summeryText: {
+      fontFamily: 'open-sans-bold',
+      fontSize: 18
+  },
+  amount: {
+      color: Colors.accent
+  }
+});
+
+export default CartScreen;
