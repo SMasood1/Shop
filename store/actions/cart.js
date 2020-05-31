95ecc5b54e9560c069fb98fd88db36f2a78b3812
Muhammad Masood <1masoodsha1@gmail.com>
2020-05-31 02:20:39 -0400

    beautify code (format)

--- store/actions/cart.js
+++ store/actions/cart.js
@@ -1,13 +1,13 @@
-export const ADD_TO_CART = 'ADD_TO_CART';
-export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const ADD_TO_CART = "ADD_TO_CART";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
 
-export const addToCart = (addedProduct)=>{
-    return {
-        type: ADD_TO_CART,
-        product: addedProduct
-    }
+export const addToCart = addedProduct => {
+  return {
+    type: ADD_TO_CART,
+    product: addedProduct
+  };
 };
 
 export const removeFromCart = productId => {
-    return {type: REMOVE_FROM_CART, pid: productId};
-}
+  return { type: REMOVE_FROM_CART, pid: productId };
+};
