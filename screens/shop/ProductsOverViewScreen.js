a6134f1e1176f1bd4e7263594797d385beaa24a9
Muhammad Masood <1masoodsha1@gmail.com>
2020-06-08 07:08:20 -0400

    add dispatch action for fetching products, add loading spinner and preform error handling

--- screens/shop/ProductsOverViewScreen.js
+++ screens/shop/ProductsOverViewScreen.js
@@ -1,14 +1,42 @@
-import React from "react";
-import { FlatList, Button } from "react-native";
+import React, { useEffect, useState, useCallback } from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  FlatList,
+  Button,
+  ActivityIndicator,
+} from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 
 import ProductItem from "../../components/shop/ProductItem";
 import * as cartActions from "../../store/actions/cart";
+import * as productActions from "../../store/actions/products";
 import Colors from "../../constants/Colors";
 
 const ProductsOverViewScreen = props => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
   const products = useSelector(state => state.products.availableProducts);
   const dispatch = useDispatch();
+
+  const loadProducts = useCallback(async () => {
+    setError(null);
+    setIsLoading(true);
+    try{
+      await dispatch(productActions.fetchProducts());
+    }
+    catch(err){
+      setError(err.message);
+    }
+    setIsLoading(false);
+  }, [dispatch, setIsLoading, setError]);
+
+  useEffect(() => {
+    
+    loadProducts();
+  }, [loadProducts, dispatch]);
 
   const selectItemHandler = (id, title) => {
     props.navigation.navigate("ProductDetail", {
@@ -16,6 +44,29 @@ const ProductsOverViewScreen = props => {
       productTitle: title
     });
   };
+
+  if (error) {
+    return (
+      <View style={styles.centered}>
+        <Text>An error occured!</Text>
+        <Button title='Try again' onPress={loadProducts} color={Colors.primary}></Button>
+      </View>
+    );
+  }
+  if (isLoading) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size="large" color={Colors.primary} />
+      </View>
+    );
+  }
+  if (!isLoading && products.length === 0) {
+    return (
+      <View style={styles.centered}>
+        <Text>No products found, maybe start adding some!</Text>
+      </View>
+    );
+  }
   return (
     <FlatList
       data={products}
@@ -45,8 +96,16 @@ const ProductsOverViewScreen = props => {
         </ProductItem>
       )}
       keyExtractor={item => item.id}
-    ></FlatList>
+    />
   );
 };
+
+const styles = StyleSheet.create({
+  centered: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center"
+  }
+});
 
 export default ProductsOverViewScreen;
